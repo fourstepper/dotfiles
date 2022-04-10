7ad7b6371de01b47d2b6a5cd86f548baccaa4708
Robin Opletal <me@robinopletal.com>
2022-04-11 01:46:13 +0200

    edit lsp diagnostics signs in the gutter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -257,15 +257,20 @@ require'lspconfig'.dockerls.setup{
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- virtual_text = {
-    --     prefix = "●",
-    --     spacing = 5,
-    -- },
-    virtual_text = false,
+    virtual_text = {
+        prefix = "●",
+        spacing = 5,
+    },
     signs = true,
     underline = true,
     update_in_insert = false,
 })
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 require'nvim-treesitter.configs'.setup {
   highlight = {
