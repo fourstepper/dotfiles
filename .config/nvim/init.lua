cc6d5ad0df42575030ed226f8a297d1f9ce83aa3
Robin Opletal <me@robinopletal.com>
2022-05-26 15:09:26 +0200

    git: move from vim-gitgutter to gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,7 +42,7 @@ require("paq")({
 	"tpope/vim-repeat",
 	"tpope/vim-surround",
         "tpope/vim-fugitive",
-        "airblade/vim-gitgutter",
+        "lewis6991/gitsigns.nvim",
 	"wellle/targets.vim",
 	"windwp/nvim-autopairs",
         "christoomey/vim-tmux-navigator",
@@ -73,6 +73,49 @@ require('Comment').setup()
 require("indent_blankline").setup {
     char = "|",
     buftype_exclude = {"terminal"}
+}
+
+require('gitsigns').setup{
+  signs = {
+    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+  },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+  end
 }
 
 require("formatter").setup({
