4a400f44e07be5857b8a8d7cef02efab021700a7
Robin Opletal <me@robinopletal.com>
2022-05-29 13:04:20 +0200

    nvim: remove overwritten local

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,6 @@ local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g -- a table to access global variables
 local opt = vim.opt -- to set options
-local fn = vim.fn
 
 g.mapleader = " "
 opt.mouse = "a"
