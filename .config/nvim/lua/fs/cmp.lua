1bcaf9bfc32317e1ddd5fafadf026edc5b2e5fe5
Robin Opletal <me@robinopletal.com>
2023-06-27 23:12:41 +0200

    feat(nvim): change sorting of cmp results
    let's see how it goes like this for a while - got to get some feel for
    it first
    
    thanks tjdevris and lukas-reineke! :)

--- .config/nvim/lua/fs/cmp.lua
+++ .config/nvim/lua/fs/cmp.lua
@@ -43,6 +43,32 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 	},
+	-- Sorting copied from https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/completion.lua#L129
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+
+			-- Mostly taken from https://github.com/lukas-reineke/cmp-under-comparator
+			function(entry1, entry2)
+				local _, entry1_under = entry1.completion_item.label:find("^_+")
+				local _, entry2_under = entry2.completion_item.label:find("^_+")
+				entry1_under = entry1_under or 0
+				entry2_under = entry2_under or 0
+				if entry1_under > entry2_under then
+					return false
+				elseif entry1_under < entry2_under then
+					return true
+				end
+			end,
+
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 	formatting = {
 		fields = { "abbr", "menu" },
 		format = function(entry, vim_item)
