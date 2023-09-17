b02c086bffe768bfea5435f25645e0d1646ae0e7
Robin Opletal <me@robinopletal.com>
2023-09-17 14:29:13 +0200

    chore(nvim): add filetype autocmds for css, scss

--- .config/nvim/lua/fs/autocmd.lua
+++ .config/nvim/lua/fs/autocmd.lua
@@ -9,6 +9,8 @@ autocmd FileType typescript setlocal ts=8 sw=8 noexpandtab
 autocmd FileType typescriptreact setlocal ts=2 sts=2 sw=2 noexpandtab
 autocmd FileType svelte setlocal ts=2 sw=2 noexpandtab
 autocmd FileType markdown setlocal ts=2 sts=2 sw=2 noexpandtab
+autocmd FileType css setlocal ts=2 sts=2 sw=2 noexpandtab
+autocmd FileType scss setlocal ts=2 sts=2 sw=2 noexpandtab
 autocmd FileType lua setlocal ts=8 sw=8 noexpandtab
 autocmd FileType go setlocal ts=8 sw=8 noexpandtab
 augroup END
