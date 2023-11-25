d646cc357be51f6d0da49833b31aa79b921ddeca
Robin Opletal <me@robinopletal.com>
2023-11-25 16:44:17 +0100

    feat(nvim): replace deprecated packer with lazy.nvim

--- .config/nvim/lua/fs/plugins.lua
+++ .config/nvim/lua/fs/plugins.lua
@@ -1,126 +1,98 @@
--- Automatically install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = vim.fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
 		"git",
 		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
-
--- Install your plugins here
-return packer.startup(function(use)
-	use({
+require("lazy").setup({
+	{
 		"iamcco/markdown-preview.nvim",
-		run = function()
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
-	})
-	use("NLKNguyen/papercolor-theme")
-	use("xiyaowong/nvim-colorizer.lua")
-	use("nvim-lualine/lualine.nvim")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
-	use("hrsh7th/nvim-cmp")
-	use("L3MON4D3/LuaSnip")
-	use("rafamadriz/friendly-snippets")
-	use({
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
+	"NLKNguyen/papercolor-theme",
+	"xiyaowong/nvim-colorizer.lua",
+	"nvim-lualine/lualine.nvim",
+	{
 		"williamboman/mason.nvim",
-		run = ":MasonUpdate", -- :MasonUpdate updates registry contents
-	})
-	use("williamboman/mason-lspconfig.nvim")
-	use("WhoIsSethDaniel/mason-tool-installer.nvim")
-	use("neovim/nvim-lspconfig")
-	use("nvim-lua/plenary.nvim")
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-fzy-native.nvim")
-	use("nvim-telescope/telescope-smart-history.nvim")
-	use("ThePrimeagen/harpoon")
-	use("tami5/sqlite.lua")
-	use({
+		build = ":MasonUpdate",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+	},
+	"neovim/nvim-lspconfig",
+	"nvim-lua/plenary.nvim",
+	"nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope-fzy-native.nvim",
+	"nvim-telescope/telescope-smart-history.nvim",
+	"ThePrimeagen/harpoon",
+	"tami5/sqlite.lua",
+	"lukas-reineke/indent-blankline.nvim",
+	"tpope/vim-repeat",
+	"tpope/vim-sleuth",
+	"declancm/maximize.nvim",
+	"tpope/vim-surround",
+	"tpope/vim-fugitive",
+	"lewis6991/gitsigns.nvim",
+	"wellle/targets.vim",
+	"windwp/nvim-autopairs",
+	"gaoDean/autolist.nvim",
+	"christoomey/vim-tmux-navigator",
+	"arouene/vim-ansible-vault",
+	"numToStr/Comment.nvim",
+	"tpope/vim-rails",
+	{
 		"nvim-treesitter/nvim-treesitter",
-		commit = "08aabb145f93ed1dd607ce8e2dcd52d356822300", -- works
-		-- commit = "823e67a1c9452075ec7f01e7aa05ac6e7b41fb1e", -- doesn't work
-		requires = {
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
 			"JoosepAlviste/nvim-ts-context-commentstring",
 			"p00f/nvim-ts-rainbow",
+			"nvim-treesitter/nvim-treesitter-context",
 		},
-		run = ":TSUpdate",
-	})
-	use({
-		"nvim-treesitter/nvim-treesitter-context",
-		requires = "nvim-treesitter/nvim-treesitter",
-	})
-	use("lukas-reineke/indent-blankline.nvim")
-	use("mhartington/formatter.nvim")
-	use({
-		"laytan/tailwind-sorter.nvim",
-		requires = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("tailwind-sorter").setup({
-				on_save_enabled = true,
-				on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.svelte" },
-			})
-		end,
-		run = "cd formatter && npm i && npm run build",
-	})
-	use({
+		build = ":TSUpdate",
+	},
+	"mhartington/formatter.nvim",
+	{
 		"nvim-tree/nvim-tree.lua",
-		requires = {
+		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
-	})
-	use({
+	},
+	{
 		"folke/trouble.nvim",
 		requires = {
 			"nvim-tree/nvim-web-devicons",
 		},
-	})
-	use("tpope/vim-repeat")
-	use("tpope/vim-sleuth")
-	use("declancm/maximize.nvim")
-	use("tpope/vim-surround")
-	use("tpope/vim-fugitive")
-	use("lewis6991/gitsigns.nvim")
-	use("wellle/targets.vim")
-	use("windwp/nvim-autopairs")
-	use("gaoDean/autolist.nvim")
-	use("christoomey/vim-tmux-navigator")
-	use("arouene/vim-ansible-vault")
-	use("numToStr/Comment.nvim")
-	use("tpope/vim-rails")
-	-- debugging
-	use("mfussenegger/nvim-dap")
-	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
-	use("nvim-telescope/telescope-dap.nvim")
-	use("leoluz/nvim-dap-go")
-	use("mfussenegger/nvim-dap-python")
-
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-telescope/telescope-dap.nvim",
+			"leoluz/nvim-dap-go",
+			"mfussenegger/nvim-dap-python",
+		},
+	},
+})
