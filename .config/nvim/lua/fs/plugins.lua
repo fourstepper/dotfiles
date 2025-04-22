4f2043ff7b22008f77fb3cf0bd305d67e8b4544f
Robin Opletal <me@robinopletal.com>
2025-04-22 15:04:13 +0200

    nvim: only use render markdown for avante

--- .config/nvim/lua/fs/plugins.lua
+++ .config/nvim/lua/fs/plugins.lua
@@ -153,9 +153,9 @@ require("lazy").setup({
 				-- Make sure to set this up properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
-					file_types = { "markdown", "Avante" },
+					file_types = { "Avante" },
 				},
-				ft = { "markdown", "Avante" },
+				ft = { "Avante" },
 			},
 		},
 	},
