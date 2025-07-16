151a8fe29d61f37df59cc11f0a9f081026b531b7
Robin Opletal <me@robinopletal.com>
2025-07-16 12:24:10 +0200

    nvim: adjust lsp config a bit

--- .config/nvim/lua/fs/lsp/config.lua
+++ .config/nvim/lua/fs/lsp/config.lua
@@ -2,21 +2,6 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protoc
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require("lspconfig").bashls.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").terraformls.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").solargraph.setup({
-	capabilities = capabilities,
-	settings = {
-		solargraph = {
-			diagnostics = false,
-			completion = true,
-		},
-	},
-})
 require("lspconfig").pylsp.setup({
 	capabilities = capabilities,
 	settings = {
@@ -33,71 +18,16 @@ require("lspconfig").pylsp.setup({
 		},
 	},
 })
-require("lspconfig").gopls.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").templ.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").ansiblels.setup({
-	capabilities = capabilities,
-})
-require("lspconfig").dockerls.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").jsonls.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").html.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").cssls.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").tailwindcss.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").eslint.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").ts_ls.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").svelte.setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig").lua_ls.setup({
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using
-				-- (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = {
-					"vim",
-					"require",
-				},
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-})
+require("lspconfig").gopls.setup({ capabilities = capabilities })
+require("lspconfig").templ.setup({ capabilities = capabilities })
+require("lspconfig").ansiblels.setup({ capabilities = capabilities })
+require("lspconfig").dockerls.setup({ capabilities = capabilities })
+require("lspconfig").jsonls.setup({ capabilities = capabilities })
+require("lspconfig").html.setup({ capabilities = capabilities })
+require("lspconfig").cssls.setup({ capabilities = capabilities })
+require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
+require("lspconfig").eslint.setup({ capabilities = capabilities })
+require("lspconfig").ts_ls.setup({ capabilities = capabilities })
+require("lspconfig").svelte.setup({ capabilities = capabilities })
+require("lspconfig").bashls.setup({ capabilities = capabilities })
+require("lspconfig").terraformls.setup({ capabilities = capabilities })
